ff8089f13a23f3219125dba07886fb3d7fdcb3ae
dean <sodp5@naver.com>
2024-02-28 14:29:07 +0900

    그림자 추가

---
+++ bezier/src/main/java/io/channel/bezier/compose/foundation/BezierShadow.kt
@@ -0,0 +1,104 @@
+package io.channel.bezier.compose.foundation
+
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.*
+import androidx.compose.ui.graphics.drawscope.ContentDrawScope
+import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
+import androidx.compose.ui.node.*
+import androidx.compose.ui.platform.InspectorInfo
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import io.channel.bezier.compose.LocalColors
+
+fun Modifier.bezierShadow(style: ShadowStyle, shape: Shape) = this.then(ShadowElement(style, shape))
+
+enum class ShadowStyle {
+    Ev1,
+    Ev2,
+    Ev3,
+}
+
+private data class ShadowElement(
+    private val shadowStyle: ShadowStyle,
+    private val shape: Shape,
+) : ModifierNodeElement<ShadowModifier>() {
+    override fun create() = ShadowModifier(
+        shadowStyle = shadowStyle,
+        shape = shape,
+    )
+
+    override fun update(node: ShadowModifier) {
+        node.shadowStyle = shadowStyle
+        node.shape = shape
+    }
+
+    override fun InspectorInfo.inspectableProperties() {
+        name = "shadow"
+        properties["shadowStyle"] = shadowStyle
+        properties["shape"] = shape
+    }
+}
+
+private class ShadowModifier(
+    var shadowStyle: ShadowStyle,
+    var shape: Shape,
+) : Modifier.Node(), DrawModifierNode, CompositionLocalConsumerModifierNode {
+    private val transparentColor = 0x00000000
+
+    override fun ContentDrawScope.draw() {
+        val shadowInfo = shadowStyle.asShadowInfo()
+
+        drawIntoCanvas { canvas ->
+            val paint = Paint()
+            val frameworkPaint = paint.asFrameworkPaint()
+
+            frameworkPaint.color = transparentColor
+            frameworkPaint.setShadowLayer(
+                shadowInfo.blur.toPx(),
+                shadowInfo.x.toPx(),
+                shadowInfo.y.toPx(),
+                shadowInfo.color.toArgb(),
+            )
+
+            val outline = shape.createOutline(size, layoutDirection, this)
+
+            canvas.drawOutline(outline, paint)
+        }
+
+        drawContent()
+    }
+
+    fun ShadowStyle.asShadowInfo(): ShadowInfo {
+        val colors = currentValueOf(LocalColors)
+
+        return when (this) {
+            ShadowStyle.Ev1 -> ShadowInfo(
+                color = colors.shdwMedium,
+                x = 0.dp,
+                y = 1.dp,
+                blur = 4.dp,
+            )
+
+            ShadowStyle.Ev2 -> ShadowInfo(
+                color = colors.shdwMedium,
+                x = 0.dp,
+                y = 2.dp,
+                blur = 6.dp,
+            )
+
+            ShadowStyle.Ev3 -> ShadowInfo(
+                color = colors.shdwLarge,
+                x = 0.dp,
+                y = 4.dp,
+                blur = 20.dp,
+            )
+        }
+    }
+
+    data class ShadowInfo(
+        val color: Color,
+        val x: Dp,
+        val y: Dp,
+        val blur: Dp,
+    )
+}
