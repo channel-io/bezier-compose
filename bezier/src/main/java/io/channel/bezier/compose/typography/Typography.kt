27ca654edaf213d5fa7113b6720e36a5d772cc80
vvvvvoin <aszx7009@gmail.com>
2024-04-30 13:56:29 +0900

    타이포그래피 클래스 추가

---
+++ bezier/src/main/java/io/channel/bezier/compose/typography/Typography.kt
@@ -0,0 +1,30 @@
+package io.channel.bezier.compose.typography
+
+import androidx.compose.ui.text.TextStyle
+
+class Typography(
+    val caption11Regular: TextStyle = TypographyToken.caption11Regular,
+    val caption11SemiBold: TextStyle = TypographyToken.caption11SemiBold,
+    val caption12Regular: TextStyle = TypographyToken.caption12Regular,
+    val caption12SemiBold: TextStyle = TypographyToken.caption12SemiBold,
+    val caption13Regular: TextStyle = TypographyToken.caption13Regular,
+    val caption13SemiBold: TextStyle = TypographyToken.caption13SemiBold,
+    val caption14Regular: TextStyle = TypographyToken.caption14Regular,
+    val caption14SemiBold: TextStyle = TypographyToken.caption14SemiBold,
+
+    val body15Regular: TextStyle = TypographyToken.body15Regular,
+    val body15SemiBold: TextStyle = TypographyToken.body15SemiBold,
+    val body16Regular: TextStyle = TypographyToken.body16Regular,
+    val body16SemiBold: TextStyle = TypographyToken.body16SemiBold,
+
+    val title17Regular: TextStyle = TypographyToken.title17Regular,
+    val title17SemiBold: TextStyle = TypographyToken.title17SemiBold,
+    val title17Bold: TextStyle = TypographyToken.title17Bold,
+    val title18Regular: TextStyle = TypographyToken.title18Regular,
+    val title18SemiBold: TextStyle = TypographyToken.title18SemiBold,
+    val title18Bold: TextStyle = TypographyToken.title18Bold,
+    val title24Regular: TextStyle = TypographyToken.title24Regular,
+    val title24Bold: TextStyle = TypographyToken.title24Bold,
+
+    val heading30Bold: TextStyle = TypographyToken.heading30Bold,
+)
