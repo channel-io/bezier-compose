a1c2c8530f32144fa0c3b4c0af03e8e21712226d
vvvvvoin <aszx7009@gmail.com>
2024-04-30 13:57:52 +0900

    베지어 테마 클래스에 타이포그래피 필드 추가

--- bezier/src/main/java/io/channel/bezier/compose/BezierTheme.kt
+++ bezier/src/main/java/io/channel/bezier/compose/BezierTheme.kt
@@ -1,17 +1,29 @@
 package io.channel.bezier.compose
 
 import androidx.compose.foundation.LocalIndication
+import androidx.compose.material.ripple.LocalRippleTheme
+import androidx.compose.material.ripple.RippleAlpha
+import androidx.compose.material.ripple.RippleTheme
+import androidx.compose.material.ripple.rememberRipple
 import androidx.compose.material3.LocalContentColor
-import androidx.compose.material.ripple.*
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.ReadOnlyComposable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.graphics.Color
-import io.channel.bezier.compose.color.*
+import io.channel.bezier.compose.color.Colors
 import io.channel.bezier.compose.color.darkColors
 import io.channel.bezier.compose.color.lightColors
+import io.channel.bezier.compose.typography.Typography
 
 @Composable
 fun BezierTheme(
-        content: @Composable () -> Unit,
+    typography: Typography = BezierTheme.typography,
+    content: @Composable () -> Unit,
 ) {
     val isDark = BezierTheme.isDark
     val colors = remember(isDark) {
@@ -22,9 +34,10 @@ fun BezierTheme(
     }
 
     CompositionLocalProvider(
-            LocalColors provides colors,
-            LocalIndication provides rememberRipple(),
-            LocalRippleTheme provides BezierRippleTheme,
+        LocalColors provides colors,
+        LocalTypography provides typography,
+        LocalIndication provides rememberRipple(),
+        LocalRippleTheme provides BezierRippleTheme,
     ) {
         content()
     }
@@ -37,21 +50,28 @@ object BezierTheme {
         @ReadOnlyComposable
         get() = LocalColors.current
 
+    val typography: Typography
+        @Composable
+        @ReadOnlyComposable
+        get() = LocalTypography.current
+
     var isDark by mutableStateOf(false)
 }
 
 internal val LocalColors = staticCompositionLocalOf { lightColors() }
 
+internal val LocalTypography = staticCompositionLocalOf { Typography() }
+
 private object BezierRippleTheme : RippleTheme {
     @Composable
     override fun defaultColor(): Color = RippleTheme.defaultRippleColor(
-            contentColor = LocalContentColor.current,
-            lightTheme = !BezierTheme.isDark,
+        contentColor = LocalContentColor.current,
+        lightTheme = !BezierTheme.isDark,
     )
 
     @Composable
     override fun rippleAlpha(): RippleAlpha = RippleTheme.defaultRippleAlpha(
-            contentColor = LocalContentColor.current,
-            lightTheme = !BezierTheme.isDark,
+        contentColor = LocalContentColor.current,
+        lightTheme = !BezierTheme.isDark,
     )
 }
