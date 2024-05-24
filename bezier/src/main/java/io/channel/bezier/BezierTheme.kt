fbd5485677879a43cf00d08f4c326bf2023bca68
vvvvvoin <aszx7009@gmail.com>
2024-05-24 13:15:52 +0900

    컨플릭트 해결

--- bezier/src/main/java/io/channel/bezier/BezierTheme.kt
+++ bezier/src/main/java/io/channel/bezier/BezierTheme.kt
@@ -1,18 +1,11 @@
 package io.channel.bezier
 
 import androidx.compose.foundation.LocalIndication
-<<<<<<< HEAD:bezier/src/main/java/io/channel/bezier/compose/BezierTheme.kt
-=======
-import androidx.compose.material.LocalContentColor
->>>>>>> upstream/v2:bezier/src/main/java/io/channel/bezier/BezierTheme.kt
 import androidx.compose.material.ripple.LocalRippleTheme
 import androidx.compose.material.ripple.RippleAlpha
 import androidx.compose.material.ripple.RippleTheme
 import androidx.compose.material.ripple.rememberRipple
-<<<<<<< HEAD:bezier/src/main/java/io/channel/bezier/compose/BezierTheme.kt
 import androidx.compose.material3.LocalContentColor
-=======
->>>>>>> upstream/v2:bezier/src/main/java/io/channel/bezier/BezierTheme.kt
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.ReadOnlyComposable
@@ -22,21 +15,15 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.graphics.Color
-<<<<<<< HEAD:bezier/src/main/java/io/channel/bezier/compose/BezierTheme.kt
-import io.channel.bezier.compose.color.Colors
-import io.channel.bezier.compose.color.darkColors
-import io.channel.bezier.compose.color.lightColors
-import io.channel.bezier.compose.typography.Typography
-=======
 import io.channel.bezier.color.Colors
 import io.channel.bezier.color.darkColors
 import io.channel.bezier.color.lightColors
->>>>>>> upstream/v2:bezier/src/main/java/io/channel/bezier/BezierTheme.kt
+import io.channel.bezier.compose.typography.Typography
 
 @Composable
 fun BezierTheme(
-    typography: Typography = BezierTheme.typography,
-    content: @Composable () -> Unit,
+        typography: Typography = BezierTheme.typography,
+        content: @Composable () -> Unit,
 ) {
     val isDark = BezierTheme.isDark
     val colors = remember(isDark) {
@@ -47,10 +34,10 @@ fun BezierTheme(
     }
 
     CompositionLocalProvider(
-        LocalColors provides colors,
-        LocalTypography provides typography,
-        LocalIndication provides rememberRipple(),
-        LocalRippleTheme provides BezierRippleTheme,
+            LocalColors provides colors,
+            LocalTypography provides typography,
+            LocalIndication provides rememberRipple(),
+            LocalRippleTheme provides BezierRippleTheme,
     ) {
         content()
     }
@@ -78,13 +65,13 @@ internal val LocalTypography = staticCompositionLocalOf { Typography() }
 private object BezierRippleTheme : RippleTheme {
     @Composable
     override fun defaultColor(): Color = RippleTheme.defaultRippleColor(
-        contentColor = LocalContentColor.current,
-        lightTheme = !BezierTheme.isDark,
+            contentColor = LocalContentColor.current,
+            lightTheme = !BezierTheme.isDark,
     )
 
     @Composable
     override fun rippleAlpha(): RippleAlpha = RippleTheme.defaultRippleAlpha(
-        contentColor = LocalContentColor.current,
-        lightTheme = !BezierTheme.isDark,
+            contentColor = LocalContentColor.current,
+            lightTheme = !BezierTheme.isDark,
     )
 }
