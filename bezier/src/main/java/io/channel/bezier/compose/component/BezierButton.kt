4d14d60ff4164827d6426dbb22e7f9cd7027920d
dean <sodp5@naver.com>
2024-05-30 14:31:14 +0900

    좌우 Contents 추가

--- bezier/src/main/java/io/channel/bezier/compose/component/BezierButton.kt
+++ bezier/src/main/java/io/channel/bezier/compose/component/BezierButton.kt
@@ -2,44 +2,72 @@ package io.channel.bezier.compose.component
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import io.channel.bezier.BezierTheme
+import io.channel.bezier.compose.R
 import io.channel.bezier.compose.color_v2.BezierColor
 
 @Composable
 fun BezierButton(
-    text: String,
-    size: BezierButtonSize,
-    variant: BezierButtonVariants,
-    color: BezierButtonColors,
-    onClick: () -> Unit,
-    modifier: Modifier = Modifier,
+        text: String,
+        size: BezierButtonSize,
+        variant: BezierButtonVariants,
+        color: BezierButtonColors,
+        onClick: () -> Unit,
+        modifier: Modifier = Modifier,
+        prefixContent: Painter? = null, // TODO : 스펙이 확장되기 이전까지는 Painter로 합니다.
+        suffixContent: Painter? = null,
 ) {
-    Box(
-        modifier = modifier
-            .clip(RoundedCornerShape(size.radius))
-            .background(getBackgroundColor(variant, color))
-            .padding(size.padding)
-            .clickable { onClick() },
+    Row(
+            modifier = modifier
+                    .clip(RoundedCornerShape(size.radius))
+                    .background(getBackgroundColor(variant, color))
+                    .padding(size.containerPadding)
+                    .clickable { onClick() },
+            verticalAlignment = Alignment.CenterVertically,
     ) {
+        if (prefixContent != null) {
+            Icon(
+                    modifier = Modifier.size(size.iconSize),
+                    painter = prefixContent,
+                    contentDescription = null,
+                    tint = getContentsColor(variant, color),
+            )
+        }
+
         Text(
-            text = text,
-            style = size.textStyle,
-            color = getContentsColor(variant, color)
+                modifier = Modifier.padding(size.textPadding),
+                text = text,
+                style = size.textStyle,
+                color = getContentsColor(variant, color),
         )
+
+        if (suffixContent != null) {
+            Icon(
+                    modifier = Modifier.size(size.iconSize),
+                    painter = suffixContent,
+                    contentDescription = null,
+                    tint = getContentsColor(variant, color),
+            )
+        }
     }
 }
 
@@ -145,13 +173,33 @@ enum class BezierButtonSize {
             XLarge -> 16.dp
         }
 
-    val padding: PaddingValues
+    val containerPadding: PaddingValues
         get() = when (this) {
             XsSmall -> PaddingValues(horizontal = 6.dp, vertical = 3.dp)
             Small -> PaddingValues(horizontal = 8.dp, vertical = 6.dp)
             Medium -> PaddingValues(horizontal = 12.dp, vertical = 9.dp)
             Large -> PaddingValues(horizontal = 14.dp, vertical = 11.dp)
             XLarge -> PaddingValues(horizontal = 16.dp, vertical = 15.dp)
+        }
+
+    val textPadding: PaddingValues
+        get() = when (this) {
+            XsSmall -> PaddingValues(horizontal = 3.dp, vertical = 0.dp)
+            Small -> PaddingValues(horizontal = 4.dp, vertical = 1.dp)
+            Medium -> PaddingValues(horizontal = 4.dp, vertical = 0.dp)
+            Large -> PaddingValues(horizontal = 5.dp, vertical = 1.dp)
+            XLarge -> PaddingValues(horizontal = 6.dp, vertical = 0.dp)
+        }
+
+    val iconSize: Dp
+        get() = when (this) {
+            XsSmall,
+            Small -> 16.dp
+
+            Medium,
+            Large -> 20.dp
+
+            XLarge -> 24.dp
         }
 }
 
@@ -174,27 +222,33 @@ private fun BezierButtonPreview() {
     BezierTheme {
         Column {
             BezierButton(
-                text = "hello world",
-                size = BezierButtonSize.Large,
-                variant = BezierButtonVariants.Primary,
-                color = BezierButtonColors.Blue,
-                onClick = {},
+                    text = "hello world",
+                    size = BezierButtonSize.Large,
+                    variant = BezierButtonVariants.Primary,
+                    color = BezierButtonColors.Blue,
+                    prefixContent = painterResource(id = R.drawable.icon_plus),
+                    suffixContent = painterResource(id = R.drawable.icon_minus_circle),
+                    onClick = {},
             )
 
             BezierButton(
-                text = "hello world",
-                size = BezierButtonSize.Large,
-                variant = BezierButtonVariants.Secondary,
-                color = BezierButtonColors.Blue,
-                onClick = {},
+                    text = "hello world",
+                    size = BezierButtonSize.Large,
+                    variant = BezierButtonVariants.Secondary,
+                    color = BezierButtonColors.Blue,
+                    prefixContent = painterResource(id = R.drawable.icon_plus),
+                    suffixContent = painterResource(id = R.drawable.icon_minus_circle),
+                    onClick = {},
             )
 
             BezierButton(
-                text = "hello world",
-                size = BezierButtonSize.Large,
-                variant = BezierButtonVariants.Tertiary,
-                color = BezierButtonColors.Blue,
-                onClick = {},
+                    text = "hello world",
+                    size = BezierButtonSize.Large,
+                    variant = BezierButtonVariants.Tertiary,
+                    color = BezierButtonColors.Blue,
+                    prefixContent = painterResource(id = R.drawable.icon_plus),
+                    suffixContent = painterResource(id = R.drawable.icon_minus_circle),
+                    onClick = {},
             )
         }
     }
