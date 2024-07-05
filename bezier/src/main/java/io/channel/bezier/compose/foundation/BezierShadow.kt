d92634feded3560997628a1f9c56251d825259fd
dean <sodp5@naver.com>
2024-07-05 19:26:33 +0900

    Shadow V2 컬러로 변경

--- bezier/src/main/java/io/channel/bezier/compose/foundation/BezierShadow.kt
+++ bezier/src/main/java/io/channel/bezier/compose/foundation/BezierShadow.kt
@@ -1,14 +1,21 @@
 package io.channel.bezier.compose.foundation
 
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.*
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Paint
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.graphics.drawOutline
 import androidx.compose.ui.graphics.drawscope.ContentDrawScope
 import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
-import androidx.compose.ui.node.*
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.node.CompositionLocalConsumerModifierNode
+import androidx.compose.ui.node.DrawModifierNode
+import androidx.compose.ui.node.ModifierNodeElement
+import androidx.compose.ui.node.currentValueOf
 import androidx.compose.ui.platform.InspectorInfo
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import io.channel.bezier.compose.LocalColors
+import io.channel.bezier.LocalColorsV2
 
 fun Modifier.bezierShadow(style: ShadowStyle, shape: Shape) = this.then(ShadowElement(style, shape))
 
@@ -22,12 +29,12 @@ enum class ShadowStyle {
 }
 
 private data class ShadowElement(
-    private val shadowStyle: ShadowStyle,
-    private val shape: Shape,
+        private val shadowStyle: ShadowStyle,
+        private val shape: Shape,
 ) : ModifierNodeElement<ShadowModifier>() {
     override fun create() = ShadowModifier(
-        shadowStyle = shadowStyle,
-        shape = shape,
+            shadowStyle = shadowStyle,
+            shape = shape,
     )
 
     override fun update(node: ShadowModifier) {
@@ -43,8 +50,8 @@ private data class ShadowElement(
 }
 
 private class ShadowModifier(
-    var shadowStyle: ShadowStyle,
-    var shape: Shape,
+        var shadowStyle: ShadowStyle,
+        var shape: Shape,
 ) : Modifier.Node(), DrawModifierNode, CompositionLocalConsumerModifierNode {
     private val transparentColor = 0x00000000
 
@@ -57,10 +64,10 @@ private class ShadowModifier(
 
             frameworkPaint.color = transparentColor
             frameworkPaint.setShadowLayer(
-                shadowInfo.blur.toPx(),
-                shadowInfo.x.toPx(),
-                shadowInfo.y.toPx(),
-                shadowInfo.color.toArgb(),
+                    shadowInfo.blur.toPx(),
+                    shadowInfo.x.toPx(),
+                    shadowInfo.y.toPx(),
+                    shadowInfo.color.toArgb(),
             )
 
             val outline = shape.createOutline(size, layoutDirection, this)
@@ -72,57 +79,57 @@ private class ShadowModifier(
     }
 
     fun ShadowStyle.asShadowInfo(): ShadowInfo {
-        val colors = currentValueOf(LocalColors)
+        val colorSchemes = currentValueOf(LocalColorsV2)
 
         return when (this) {
             ShadowStyle.Shadow1 -> ShadowInfo(
-                color = colors.shdwMedium,
-                x = 0.dp,
-                y = 1.dp,
-                blur = 4.dp,
+                    color = colorSchemes.shadowMedium.color,
+                    x = 0.dp,
+                    y = 1.dp,
+                    blur = 4.dp,
             )
 
             ShadowStyle.Shadow2 -> ShadowInfo(
-                color = colors.shdwMedium,
-                x = 0.dp,
-                y = 2.dp,
-                blur = 6.dp,
+                    color = colorSchemes.shadowMedium.color,
+                    x = 0.dp,
+                    y = 2.dp,
+                    blur = 6.dp,
             )
 
             ShadowStyle.Shadow3 -> ShadowInfo(
-                color = colors.shdwLarge,
-                x = 0.dp,
-                y = 4.dp,
-                blur = 20.dp,
+                    color = colorSchemes.shadowLarge.color,
+                    x = 0.dp,
+                    y = 4.dp,
+                    blur = 20.dp,
             )
 
             ShadowStyle.Shadow4 -> ShadowInfo(
-                color = colors.shdwXlarge,
-                x = 0.dp,
-                y = 4.dp,
-                blur = 24.dp,
+                    color = colorSchemes.shadowXlarge.color,
+                    x = 0.dp,
+                    y = 4.dp,
+                    blur = 24.dp,
             )
 
             ShadowStyle.Shadow5 -> ShadowInfo(
-                color = colors.shdwXlarge,
-                x = 0.dp,
-                y = 6.dp,
-                blur = 40.dp,
+                    color = colorSchemes.shadowXlarge.color,
+                    x = 0.dp,
+                    y = 6.dp,
+                    blur = 40.dp,
             )
 
             ShadowStyle.Shadow6 -> ShadowInfo(
-                color = colors.shdwXlarge,
-                x = 0.dp,
-                y = 12.dp,
-                blur = 60.dp,
+                    color = colorSchemes.shadowXlarge.color,
+                    x = 0.dp,
+                    y = 12.dp,
+                    blur = 60.dp,
             )
         }
     }
 
     data class ShadowInfo(
-        val color: Color,
-        val x: Dp,
-        val y: Dp,
-        val blur: Dp,
+            val color: Color,
+            val x: Dp,
+            val y: Dp,
+            val blur: Dp,
     )
 }
