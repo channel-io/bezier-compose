0e11f081f28baa617bdde1c10885ca434cfb62e1
dean <sodp5@naver.com>
2024-07-29 23:16:12 +0900

    outlineBorder 추가

--- bezier/src/main/java/io/channel/bezier/extension/ModifierExtensions.kt
+++ bezier/src/main/java/io/channel/bezier/extension/ModifierExtensions.kt
@@ -9,7 +9,6 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.composed
