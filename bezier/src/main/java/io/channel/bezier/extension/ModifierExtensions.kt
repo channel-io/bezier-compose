14c6d8fcfb9636f151562da7238a5409a5e0425f
dean <sodp5@naver.com>
2024-07-30 14:45:29 +0900

    outlineBorder 1px 보정 추가

--- bezier/src/main/java/io/channel/bezier/extension/ModifierExtensions.kt
+++ bezier/src/main/java/io/channel/bezier/extension/ModifierExtensions.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.composed
