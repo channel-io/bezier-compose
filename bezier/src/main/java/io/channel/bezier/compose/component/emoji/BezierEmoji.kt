885211976571933cee0923b6e438f556b460d35b
Boin Seo, George <58923717+vvvvvoin@users.noreply.github.com>
2024-09-03 19:50:59 +0900

    [MOB-1868] Emoji 추가 (#65)
    * BezierEmoji 추가
    
    * icon -> image 로 변경
    
    * 리뷰 반영 - 디자인과 다르게 표시될 수 있는 경우 제거

---
+++ bezier/src/main/java/io/channel/bezier/compose/component/emoji/BezierEmoji.kt
@@ -0,0 +1,81 @@
+package io.channel.bezier.compose.component.emoji
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.requiredSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import io.channel.bezier.BezierTheme
+import io.channel.bezier.compose.component.badge.chat.BezierChatBadge
+import io.channel.bezier.compose.component.emoji.properties.BezierEmojiBadge
+import io.channel.bezier.compose.component.emoji.properties.BezierEmojiSize
+import io.channel.bezier.extension.toEmojiPainter
+
+@Composable
+fun BezierEmoji(
+        name: String,
+        size: BezierEmojiSize,
+        modifier: Modifier = Modifier,
+        badge: BezierEmojiBadge = BezierEmojiBadge.None,
+) {
+    Box(
+            modifier = modifier
+                    .requiredSize(size.size),
+    ) {
+        Image(
+                modifier = Modifier.fillMaxSize(),
+                painter = name.toEmojiPainter,
+                contentDescription = null,
+        )
+
+        Box(
+                modifier = Modifier
+                        .align(Alignment.BottomEnd)
+                        .offset(x = 4.dp, y = 4.dp),
+        ) {
+            when (badge) {
+                BezierEmojiBadge.None -> Unit
+                BezierEmojiBadge.Chat -> BezierChatBadge(size = size.chatBadgeSize)
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true, widthDp = 700, heightDp = 300)
+@Composable
+private fun BezierEmojiPreview() {
+    BezierTheme {
+        Column {
+            Row(
+                    horizontalArrangement = Arrangement.spacedBy(8.dp),
+            ) {
+                BezierEmojiSize.entries.forEach { size ->
+                    BezierEmoji(
+                            name = "smile",
+                            size = size,
+                    )
+                }
+            }
+
+            Row(
+                    horizontalArrangement = Arrangement.spacedBy(8.dp),
+            ) {
+                BezierEmojiSize.entries.forEach { size ->
+                    BezierEmoji(
+                            name = "smile",
+                            size = size,
+                            badge = BezierEmojiBadge.Chat,
+                    )
+                }
+            }
+        }
+    }
+}
