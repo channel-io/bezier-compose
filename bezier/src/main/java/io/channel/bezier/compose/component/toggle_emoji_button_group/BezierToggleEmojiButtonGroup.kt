c80f182598b660f593c96c1e056fb846ccd3393f
vvvvvoin <aszx7009@gmail.com>
2024-09-11 19:23:10 +0900

    토글 스펙 누락 업데이트

--- bezier/src/main/java/io/channel/bezier/compose/component/toggle_emoji_button_group/BezierToggleEmojiButtonGroup.kt
+++ bezier/src/main/java/io/channel/bezier/compose/component/toggle_emoji_button_group/BezierToggleEmojiButtonGroup.kt
@@ -5,23 +5,29 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import io.channel.bezier.BezierTheme
+import io.channel.bezier.compose.component.toggle_emoji_button_group.property.BezierToggleEmojiButtonGroupFillDirectionType
 import io.channel.bezier.compose.component.toggle_emoji_button_group.source.toggle_emoji_button.BezierToggleEmojiButton
 import io.channel.bezier.compose.component.toggle_emoji_button_group.source.toggle_emoji_button.properties.BezierToggleEmojiButtonVariant
-import io.channel.bezier.compose.component.toggle_emoji_button_group.property.BezierToggleEmojiButtonGroupFillDirectionType
 import io.channel.bezier.extension.thenIf
 
 @Composable
 fun BezierToggleEmojiButtonGroup(
         emojis: List<String>,
         type: BezierToggleEmojiButtonGroupFillDirectionType,
-        onEmojiClick: (String) -> Unit,
+        onEmojiClick: (index: Int) -> Unit,
         modifier: Modifier = Modifier,
+        selectedIndex: Int? = null,
 ) {
     val horizontalArrangement = when (type) {
         BezierToggleEmojiButtonGroupFillDirectionType.Undefined -> Arrangement.spacedBy(
@@ -37,12 +43,12 @@ fun BezierToggleEmojiButtonGroup(
             modifier = modifier,
             horizontalArrangement = horizontalArrangement,
     ) {
-        emojis.forEach { emoji ->
+        emojis.forEachIndexed { index, emoji ->
             BezierToggleEmojiButton(
-                    selected = false,
+                    selected = index == selectedIndex,
                     name = emoji,
                     variant = BezierToggleEmojiButtonVariant.Primary,
-                    onClick = { onEmojiClick.invoke(emoji) },
+                    onClick = { onEmojiClick.invoke(index) },
                     modifier = Modifier
                             .thenIf(type == BezierToggleEmojiButtonGroupFillDirectionType.All) {
                                 aspectRatio(1f).weight(1f)
@@ -60,24 +66,33 @@ fun BezierToggleEmojiButtonGroup(
 private fun BezierToggleEmojiButtonGroupPreview() {
     BezierTheme {
         Column(
+                modifier = Modifier.padding(10.dp),
                 verticalArrangement = Arrangement.spacedBy(10.dp),
         ) {
+            val emojis = remember { listOf("1", "2", "3", "4") }
+            var selectedIndex by remember { mutableStateOf<Int?>(null) }
+
             BezierToggleEmojiButtonGroup(
-                    emojis = listOf("1", "2", "3", "4"),
+                    emojis = emojis,
                     type = BezierToggleEmojiButtonGroupFillDirectionType.Undefined,
-                    onEmojiClick = {},
+                    onEmojiClick = { selectedIndex = it },
+                    selectedIndex = selectedIndex,
                     modifier = Modifier.fillMaxWidth(),
             )
+
             BezierToggleEmojiButtonGroup(
-                    emojis = listOf("1", "2", "3", "4"),
+                    emojis = emojis,
                     type = BezierToggleEmojiButtonGroupFillDirectionType.Horizontal,
-                    onEmojiClick = {},
+                    onEmojiClick = { selectedIndex = it },
+                    selectedIndex = selectedIndex,
                     modifier = Modifier.fillMaxWidth(),
             )
+
             BezierToggleEmojiButtonGroup(
-                    emojis = listOf("1", "2", "3", "4"),
+                    emojis = emojis,
                     type = BezierToggleEmojiButtonGroupFillDirectionType.All,
-                    onEmojiClick = {},
+                    onEmojiClick = { selectedIndex = it },
+                    selectedIndex = selectedIndex,
                     modifier = Modifier.fillMaxWidth(),
             )
         }
