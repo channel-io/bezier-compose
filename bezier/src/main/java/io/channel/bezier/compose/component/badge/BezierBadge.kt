9058addbbcc955c98f4f895751618c4168557252
dean <sodp5@naver.com>
2024-09-24 16:29:29 +0900

    BezierBadge 추가

---
+++ bezier/src/main/java/io/channel/bezier/compose/component/badge/BezierBadge.kt
@@ -0,0 +1,86 @@
+package io.channel.bezier.compose.component.badge
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Icon
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import io.channel.bezier.BezierIcon
+import io.channel.bezier.BezierIcons
+import io.channel.bezier.BezierTheme
+import io.channel.bezier.compose.component.badge.properties.BezierBadgeColor
+import io.channel.bezier.compose.component.badge.properties.BezierBadgeSize
+import io.channel.bezier.icon.CircleSmall
+
+@Composable
+fun BezierBadge(
+        text: String,
+        size: BezierBadgeSize,
+        color: BezierBadgeColor,
+        modifier: Modifier = Modifier,
+        icon: BezierIcon? = null,
+) {
+    Row(
+            modifier = modifier
+                    .clip(RoundedCornerShape(size.radius))
+                    .background(color.backgroundColor().color)
+                    .padding(size.backgroundPadding),
+            horizontalArrangement = Arrangement.spacedBy(size.contentGap),
+            verticalAlignment = Alignment.CenterVertically,
+    ) {
+        if (icon != null) {
+            Icon(
+                    modifier = Modifier.size(size.iconSize),
+                    imageVector = icon.imageVector,
+                    contentDescription = icon.imageVector.name,
+                    tint = color.contentColor().color,
+            )
+        }
+
+        Text(
+                modifier = Modifier.padding(size.textPadding),
+                text = text,
+                style = size.textStyle(),
+                color = color.contentColor().color,
+        )
+    }
+}
+
+@Composable
+@Preview(
+        showBackground = true,
+        widthDp = 1300,
+)
+fun BezierBadgePreview() {
+    BezierTheme {
+        Column(
+                modifier = Modifier.padding(16.dp),
+                verticalArrangement = Arrangement.spacedBy(8.dp),
+        ) {
+            BezierBadgeSize.entries.forEach { size ->
+                Row(
+                        horizontalArrangement = Arrangement.spacedBy(8.dp),
+                ) {
+                    BezierBadgeColor.entries.forEach { color ->
+                        BezierBadge(
+                                text = "Label",
+                                size = size,
+                                color = color,
+                                icon = BezierIcons.CircleSmall,
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ bezier/src/main/java/io/channel/bezier/compose/component/badge/properties/BezierBadgeColor.kt
@@ -0,0 +1,63 @@
+package io.channel.bezier.compose.component.badge.properties
+
+import androidx.compose.runtime.Composable
+import io.channel.bezier.BezierTheme
+import io.channel.bezier.compose.color_v2.BezierColor
+
+enum class BezierBadgeColor(
+        val backgroundColor: @Composable () -> BezierColor,
+        val contentColor: @Composable () -> BezierColor,
+) {
+    Grey(
+            backgroundColor = { BezierTheme.colorSchemes.bgBlackLighter },
+            contentColor = { BezierTheme.colorSchemes.fgBlackDarker },
+    ),
+    DarkGrey(
+            backgroundColor = { BezierTheme.colorSchemes.bgBlackDarker },
+            contentColor = { BezierTheme.colorSchemes.fgAbsoluteWhiteDark },
+    ),
+    Blue(
+            backgroundColor = { BezierTheme.colorSchemes.bgBlueShadeLight },
+            contentColor = { BezierTheme.colorSchemes.fgBlueDark },
+    ),
+    Cobalt(
+            backgroundColor = { BezierTheme.colorSchemes.bgCobaltShadeLight },
+            contentColor = { BezierTheme.colorSchemes.fgCobaltDark },
+    ),
+    Teal(
+            backgroundColor = { BezierTheme.colorSchemes.bgTealShadeLight },
+            contentColor = { BezierTheme.colorSchemes.fgTealDark },
+    ),
+    Green(
+            backgroundColor = { BezierTheme.colorSchemes.bgGreenShadeLight },
+            contentColor = { BezierTheme.colorSchemes.successFgDark },
+    ),
+    Olive(
+            backgroundColor = { BezierTheme.colorSchemes.bgOliveShadeLight },
+            contentColor = { BezierTheme.colorSchemes.fgOliveDark },
+    ),
+    Pink(
+            backgroundColor = { BezierTheme.colorSchemes.bgPinkShadeLight },
+            contentColor = { BezierTheme.colorSchemes.fgPinkDark },
+    ),
+    Navy(
+            backgroundColor = { BezierTheme.colorSchemes.bgNavyShadeLight },
+            contentColor = { BezierTheme.colorSchemes.fgNavyDark },
+    ),
+    Yellow(
+            backgroundColor = { BezierTheme.colorSchemes.bgYellowShadeLight },
+            contentColor = { BezierTheme.colorSchemes.fgYellowDark },
+    ),
+    Orange(
+            backgroundColor = { BezierTheme.colorSchemes.bgOrangeShadeLight },
+            contentColor = { BezierTheme.colorSchemes.fgOrangeDark },
+    ),
+    Red(
+            backgroundColor = { BezierTheme.colorSchemes.bgRedShadeLight },
+            contentColor = { BezierTheme.colorSchemes.criticalFgDark },
+    ),
+    Purple(
+            backgroundColor = { BezierTheme.colorSchemes.bgPurpleShadeLight },
+            contentColor = { BezierTheme.colorSchemes.fgPurpleDark },
+    ),
+}
